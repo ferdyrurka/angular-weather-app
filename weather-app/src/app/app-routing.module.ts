ea41870e584fa680ab0fff367daffdf896302433
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-04-05 23:14:12 +0200

    Update default component

--- weather-app/src/app/app-routing.module.ts
+++ weather-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home/component/home/home.component";
+import {CurrentWeatherComponent} from "./weather/component/current-weather/current-weather.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'weather/:cityName', component: CurrentWeatherComponent }
 ];
 
 @NgModule({
--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatMenuModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from "@angular/material";
+import {
+  MatMenuModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule
+} from "@angular/material";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { LayoutModule } from '@angular/cdk/layout';
 import { HomeComponent } from './home/component/home/home.component';
+import { CurrentWeatherComponent } from './weather/component/current-weather/current-weather.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CurrentWeatherComponent
   ],
   imports: [
     BrowserModule,
