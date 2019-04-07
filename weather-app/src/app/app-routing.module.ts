a7fde4ad34fd1b6ec20f4f8f9d50124ceeb1e217
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-04-07 23:16:53 +0200

    routing

--- weather-app/src/app/app-routing.module.ts
+++ weather-app/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./home/component/home/home.component";
-import {CurrentWeatherComponent} from "./weather/component/current-weather/current-weather.component";
+import {HomeComponent} from "./home/home/home.component";
+import {CurrentWeatherComponent} from "./weather/current-weather/current-weather.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
