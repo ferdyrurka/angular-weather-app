2203a986612016074baf76de7ab9ed0cf0aaabf5
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-04-11 20:47:08 +0200

    Refactoring

--- weather-app/src/app/weather/weather/weather.component.ts
+++ weather-app/src/app/weather/weather/weather.component.ts
@@ -2,15 +2,15 @@ import {Component, OnInit} from '@angular/core';
 import {APIClientOWMService} from "../../core/service/apiclient-owm.service";
 import {SnackBarErrorService} from "../../core/service/snack-bar-error.service";
 import {Subscription} from "rxjs";
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {SearchWeather} from "../../core/model/search-weather";
 
 @Component({
   selector: 'app-current-weather',
-  templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.css']
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
 })
-export class CurrentWeatherComponent implements OnInit {
+export class WeatherComponent implements OnInit {
 
   private weather = null;
 
@@ -23,13 +23,14 @@ export class CurrentWeatherComponent implements OnInit {
   private searchWeather:SearchWeather;
 
   constructor(
-    private router:ActivatedRoute,
+    private activatedRouter:ActivatedRoute,
+    private router:Router,
     private apiClientOWMService:APIClientOWMService,
     private snackBarError:SnackBarErrorService
   ) {}
 
   ngOnInit() {
-    this.cityName = this.router.snapshot.paramMap.get('cityName');
+    this.cityName = this.activatedRouter.snapshot.paramMap.get('cityName');
     this.searchCurrentWeather(this.cityName);
     this.searchWeather = new SearchWeather(this.cityName);
   }
@@ -41,12 +42,20 @@ export class CurrentWeatherComponent implements OnInit {
     this.weatherSubscription = this.apiClientOWMService.getWeather(APIClientOWMService.CURRENT_WEATHER, cityName)
       .subscribe((res) => {
         this.cityName = cityName;
+        this.refreshUrl(this.cityName);
+
         this.weather = res.body as Object;
+
         this.weatherSubscription.unsubscribe();
       }, error =>  {
         this.errorBlockShow = true;
+
         this.snackBarError.openSnackBarError(error.message);
       })
     ;
+  }
+
+  private refreshUrl(cityName:string): void {
+    this.router.navigate(['weather', cityName]);
   }
 }
