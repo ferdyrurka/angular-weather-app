a685d84235e15f4d90c4d4ba6e300a9fdfb6193d
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-04-16 21:02:40 +0200

    e2e

--- weather-app/e2e/src/app.e2e-spec.ts
+++ weather-app/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import {beforeEach} from "selenium-webdriver/testing";
 
 describe('workspace-project App', () => {
   let page: AppPage;
